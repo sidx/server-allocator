3b04139d2dd8edd59119bddb9bf7010a3f680ca5
sidx <sidharth.xtb@gmail.com>
2016-10-03 02:42:58 +0530

    Completed all 3 scenarios. Changed output to proper format.

--- allocator.py
+++ allocator.py
@@ -1,12 +1,4 @@
-class region:
-	def __init__(self,regionName,servers):
-		self.name = regionName
-		availableServers = []
-		for i in servers:
-			availableServers.append(server(servers[i],i)
-		self.priceSortedServers = sorted(priceSortedServers,key=lambda ser:ser.price_per_cpu)
-		
-		#add server class, add sorting function
+
 
 class server:
 	def __init__(self,price,server_type):
@@ -25,34 +17,68 @@ class server:
 		elif self.server_type == '10xlarge':
 			self.cpus = 32
 		else:
-			raise Exception('Server type "'+self.server_type+'" not recogonized')
+			raise Exception('Server type "'+self.server_type+'" not recognised')
 		self.price_per_cpu = self.price/self.cpus
+
+
+class region:
+	def __init__(self,regionName,servers):
+		self.name = regionName
+		self.availableServers = []
+		for i in servers:
+			self.availableServers.append(server(servers[i],i))
+		self.priceSortedServers = sorted(self.availableServers,key=lambda ser:ser.price_per_cpu)
 		
-def calculateCostWithCpus(regionName,priceList,totalCount):
+def calculateCostWithCpus(regionName,priceList,totalCount,hours):
 	regionServers = region(regionName,priceList)
 	result = []
 	serverLeft = totalCount
+	totalCost = 0
 	sortedServerStack = list(regionServers.priceSortedServers)
 	while serverLeft > 0:
 		if sortedServerStack[0].cpus <= serverLeft and sortedServerStack[0].price <= sortedServerStack[1].price:
-			result.append((i.server_type,(serverLeft/i.cpus) + 1))
+			result.append((sortedServerStack[0].server_type,int(serverLeft/sortedServerStack[0].cpus) + 1))
+			totalCost += (int(serverLeft/sortedServerStack[0].cpus) + 1)*sortedServerStack[0].price
 			serverLeft = 0
 		elif sortedServerStack[0].cpus <= serverLeft:
-			result.append((i.server_type, serverLeft/i.cpus))
-			serverLeft = serverLeft % i.cpus
-			
+			result.append((sortedServerStack[0].server_type, int(serverLeft/sortedServerStack[0].cpus)))
+			serverLeft = serverLeft % sortedServerStack[0].cpus
+			totalCost += (int(serverLeft/sortedServerStack[0].cpus))*sortedServerStack[0].price
 		sortedServerStack.pop(0)
-	
-	return result
+	return {'region':regionName, 'total_cost':totalCost * hours, 'servers':result}
 
 
 def calculateCostWithPrice(regionName,priceList,maxPrice,hours):
 	regionServers = region(regionName,priceList)
 	result = []
-	serverLeft = totalCount
-	sortedServerStack = list(regionServers.priceSortedServers)
-	pricePerHour = maxPrice/hours
+	#serverLeft = totalCount
+	sortedServerStack = list(sorted(regionServers.availableServers,key=lambda ser:ser.price))
+	pricePerHour = float(maxPrice)/float(hours)
+	totalCost = 0
+	while pricePerHour > 0 and sortedServerStack:
+		if sortedServerStack[0].price <= pricePerHour:
+			totalCost += int(pricePerHour/sortedServerStack[0].price) * sortedServerStack[0].price
+			result.append((sortedServerStack[0].server_type, int(pricePerHour/sortedServerStack[0].price)))
+			pricePerHour = pricePerHour % sortedServerStack[0].price
+		sortedServerStack.pop(0)
 	
+	return {'region':regionName, 'total_cost':totalCost*hours, 'servers':result}
+
+def calculateCostWithPriceAndCpu(regionName,priceList,maxPrice,minCpu, hours):
+	regionServers = region(regionName,priceList)
+	cpuHired = 0
+	moneyLeft = float(maxPrice)/float(hours)
+	result = []
+	totalCost = 0
+	cpuPriceSortedList = list(regionServers.priceSortedServers)
+	while cpuHired < minCpu and cpuPriceSortedList:
+		if cpuPriceSortedList[0].price <= moneyLeft:
+			cpuHired += cpuPriceSortedList[0].cpus * int(moneyLeft/cpuPriceSortedList[0].price)
+			totalCost += int(moneyLeft/cpuPriceSortedList[0].price) * cpuPriceSortedList[0].price
+			result.append((cpuPriceSortedList[0].server_type,int(moneyLeft/cpuPriceSortedList[0].price)))
+			moneyLeft = moneyLeft % cpuPriceSortedList[0].price
+		cpuPriceSortedList.pop(0)
+        return {'region':regionName, 'total_cost':totalCost*hours,'servers':result}
 
 
 def get_costs(instances,hours,cpus=None,price=None):
@@ -60,8 +86,16 @@ def get_costs(instances,hours,cpus=None,price=None):
 		raise Exception("Error. No.of cpus and max price can't be both empty")
 		return
 	result = []
-	for regionName in instances:
-		resultDict = calculateCostWithCpus(regionName,instances[regionName],cpus)
+	if price and cpus:
+		for regionName in instances:
+			result.append(calculateCostWithPriceAndCpu(regionName,instances[regionName],price,cpus,hours))
+	elif price:
+		for regionName in instances:
+			result.append(calculateCostWithPrice(regionName, instances[regionName],price,hours))
+	elif cpus:
+		for regionName in instances:
+			result.append(calculateCostWithCpus(regionName, instances[regionName], cpus,hours))
+	return result
 		
 	
 	
